0a2264bb58a9ba1f612491ae1e75b56cdc61800a
Guanchor Ojeda Hernández <gojeda@inventiaplus.com>
2015-11-03 08:24:55 +0000

    + xety/cake3-upload install: composer | bootstrap | gitinore

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -182,6 +182,7 @@ Request::addDetector('tablet', function ($request) {
 
 Plugin::load('Migrations');
 Plugin::load('AdminLTEBakeOverride');
+Plugin::load('Xety/Cake3Upload');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
